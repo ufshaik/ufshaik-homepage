2ab9300d5af6e8a408309a9f841005af7d8448ae
Umar Shaik <ufshaikc@gmail.com>
2021-11-11 13:21:39 -0330

    Add the model and enable renderer resizing

--- components/paint-dvt.js
+++ components/paint-dvt.js
@@ -11,6 +11,109 @@ function easeOutCircle(x){
 const PaintDvt = () =>{
   const refContainer = useRef()
   const [loading, setLoading] = useState(true)
+  const [renderer, setRenderer] = useState()
+  const [_camera, setCamera] = useState()
+  const [target] = useState(new THREE.Vector3(-0.5, 1.2, 0))
+  const [initialCameraPosition] = useState(
+    new THREE.Vector3(
+      20 * Math.sin(0.2 * Math.PI),
+      10,
+      20 * Math.cos(0.2 * Math.PI)
+    )
+  )
+
+  const [scene] = useState(new THREE.Scene())
+  const [_controls, setControls] = useState()
+
+  const handleWindowResize = useCallback( () =>{
+    const {current: container } = refContainer
+    if (container && renderer){
+      const scW = container.clientWidth
+      const scH = container.clientHeight
+
+      renderer.setSize(scW, scH)
+    }
+  }, [renderer])
+
+  /* eslint-disable react-hooks/exhaustive-deps */
+
+  useEffect( () =>{
+    const { current: container } = refContainer
+    if (container && !renderer){
+      const scW = container.clientWidth
+      const scH = container.clientHeight
+      const renderer = new THREE.WebGLRenderer({
+        antialias: true,
+        alpha: true
+      })
+      renderer.setPixelRatio(window.devicePixelRatio)
+      renderer.setSize(scW, scH)
+      renderer.outputEncoding = THREE.sRGBEncoding
+      container.appendChild(renderer.domElement)
+      setRenderer(renderer)
+
+      // 640 -> 240
+      // 8 -> 6
+      const scale = scH * 0.04 + 2
+      const camera = new THREE.OrthographicCamera(
+        -scale,
+          scale,
+        scale,
+          -scale,
+        0.01,
+        50000
+      )
+      camera.position.copy( initialCameraPosition)
+      camera.lookAt(target)
+      setCamera(camera)
+
+      const ambientLight = new THREE.AmbientLight(0xcccccc,1)
+      scene.add(ambientLight)
+
+      const controls = new OrbitControls(camera, renderer.domElement)
+      controls.autoRotate = true
+      controls.target = target
+      setControls(controls)
+
+      loadGLTFModel(scene, '/Tororo.glb', {
+        receiveShadow: false,
+        castShadow: false
+      }).then( () =>{
+        animate()
+        setLoading(false)
+      })
+      let req = null
+      let frame = 0
+      const animate = () => {
+          req = requestAnimationFrame(animate)
+
+          frame = frame <= 100 ? frame + 1 : frame
+          if(frame <= 100) {
+            const p = initialCameraPosition
+            const rotateSpeed = -easeOutCircle(frame / 120) * Math.PI * 20
+            camera.position.y = 5
+            camera.position.x = p.x * Math.cos(rotateSpeed) + p.z * Math.sin(rotateSpeed)
+            camera.position.z = p.z * Math.cos(rotateSpeed) - p.x * Math.sin(rotateSpeed)
+            camera.lookAt(target)
+          }  else{
+            controls.update()
+          }
+
+          renderer.render(scene, camera)
+      }
+      return ( ) =>{
+        cancelAnimationFrame(req)
+        renderer.dispose()
+      }
+    }
+  }, [])
+
+  useEffect( () => {
+    window.addEventListener('resize', handleWindowResize, false)
+    return () =>{
+      window.removeEventListener('resize', handleWindowResize, false)
+    }
+  }, [renderer, handleWindowResize])
 
   return(
     <Box ref={refContainer} className={"paint-dvt"} m={"auto"}
@@ -21,10 +124,14 @@ const PaintDvt = () =>{
          position={"relative"}
     >
       { loading && (
-        <Spinner size={"xl"} position={"absolute"} left={"50%"}
-                 top={"50%"} ml={"calc(0px - var(--spinner-size) / 2 )"} mt={"calc(0px - var(--spinner-size)"} />
+        <Spinner
+          size={"xl"}
+          position={"absolute"}
+          left={"50%"}
+          top={"50%"}
+          ml={"calc(0px - var(--spinner-size) / 2 )"}
+          mt={"calc(0px - var(--spinner-size)"} />
       )}
-      Dragon Vs Troll !!
     </Box>
   )
 }
